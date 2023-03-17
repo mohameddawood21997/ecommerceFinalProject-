a27d98eb8717065168c4e08ef7b153368023f740
mostafa220 <mostafasharkawy432@gmail.com>
2023-03-18 00:31:16 +0200

    update

---
+++ database/migrations/2023_03_16_071849_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->string('rate')->nullable();
+            $table->string('price');
+            $table->string('quantity');
+            $table->string('status');
+            $table->string('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
